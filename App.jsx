6cfd550c076d161ddd4bf38a8cfc4426de0aced5
Gabriel <gabrielsb1090@gmail.com>
2026-03-20 20:33:36 -0300

    Criando estrutura de tarefas

--- App.jsx
+++ App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react"
 
 function App() {
+
+  const [tarefa, setTarefa] = useState([])
+  const [valor, setValor] = useState("")
+
+  function enviarTarefa(){
+    setTarefa([...tarefa, valor])
+
+    console.log(tarefa)
+  }
 
   return (
     <>
       <input 
       type="text" 
-      placeholder="adicione tarefa"/>
+      placeholder="adicione tarefa"
+      value={valor}
+      onChange={(e)=> setValor(e.target.value)}/>
+
+      <button onClick={enviarTarefa}>Adicionar atrefa</button>
+      {tarefa.map((tarefa, index) =>{
+        return (
+           <ul>
+            <li key={index}>{tarefa}</li>
+           </ul>
+        )
+      })}
     </>
   )
 }
