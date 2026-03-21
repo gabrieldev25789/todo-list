4297038c86bf6c693a3d2d76f3cf5efe06b78c63
Gabriel <gabrielsb1090@gmail.com>
2026-03-21 12:33:56 -0300

    Estruturando header

---
+++ Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <>
+    <div id="img">
+        <h1>Organize seu dia</h1>
+        <p>Anote o que precisa fazer e deixe nada passar.</p>
+    </div>
+    </>
+  )
+}
+
+export default Header
